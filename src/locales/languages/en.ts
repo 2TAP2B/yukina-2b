206047eaedaf899291d492327a605c0345a07c31
Tobi <steltner@pm.me>
2025-05-23 10:50:03 +0200

    modified:   src/layouts/PostLayout.astro 	modified:   src/locales/languages/en.ts

--- src/locales/languages/en.ts
+++ src/locales/languages/en.ts
@@ -2,9 +2,9 @@ import type { Translation } from "../translation";
 import key from "../keys";
 
 export const en: Translation = {
-  [key.copy_right_author]: "Author",
-  [key.copy_right_publish_date]: "Publish Date",
-  [key.copy_right_license]: "License",
+  [key.copy_right_author]: "Autor",
+  [key.copy_right_publish_date]: "Datum",
+  [key.copy_right_license]: "Lizenz",
 
   [key.nav_bar_home]: "Home",
   [key.nav_bar_archive]: "Archive",
@@ -13,19 +13,19 @@ export const en: Translation = {
   [key.nav_bar_github]: "GitHub",
   [key.nav_bar_search_placeholder]: "Search",
 
-  [key.post_card_words]: "Words",
-  [key.post_card_minutes]: "Minutes",
+  [key.post_card_words]: "Wörter",
+  [key.post_card_minutes]: "Minuten",
 
-  [key.side_bar_categories]: "Categories",
+  [key.side_bar_categories]: "Kategorien",
   [key.side_bar_tags]: "Tags",
-  [key.side_bar_view_more]: "View More",
+  [key.side_bar_view_more]: "Mehr",
 
-  [key.archive_year_title_count]: "Total {{}} article(s)",
+  [key.archive_year_title_count]: "Insgesamt {{}} Artikel",
 
-  [key.pages_categories_archive]: "Article Archive",
-  [key.pages_tags_archive]: "Tag Archive",
-  [key.pages_archive_archive]: "Archive",
+  [key.pages_categories_archive]: "Artikel Archiv",
+  [key.pages_tags_archive]: "Tag Archiv",
+  [key.pages_archive_archive]: "Archiv",
 
   [key.pages_tags_title]: "Tags",
-  [key.pages_categories_title]: "Categories",
+  [key.pages_categories_title]: "Kategorien",
 };
